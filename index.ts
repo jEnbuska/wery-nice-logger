209afec23e40294070a94bab1522e0a7125ac503
Joona Enbuska <joona.enbuska@eficode.com>
2019-03-09 11:17:55 +0200

    create playground, text formatting improvements

--- index.ts
+++ index.ts
@@ -4,31 +4,16 @@ export type NiceLoggerFilter = ({stackInfo, args, func}: {stackInfo: NiceStack, 
 export type NiceLoggerFormatter = ({stackInfo, args, func}: {stackInfo: NiceStack, args: any[], func: NiceloggerLogLevel}) => any;
 export type NiceStack = ({file: string, func: string, char: string, line: string})[]
 
-const filePath = require.main!.filename.split('/')
-filePath.pop();
-const mainDirectory = filePath.join('/');
-
-export function createStackInfo(): NiceStack {
-    const ctx: {stack?: string} = {}
-    Error.captureStackTrace(ctx);
-    const rows = ctx.stack!.split('\n').splice(3).map(row => row.trim())
-    const out = [];
-    for(const row of rows) {
-        const [_, func, ...rest] = row.split(' ');
-        const position = rest.join();
-        const [char, line, ...filePath] = position.substring(0, position.length-1).split(':').reverse();
-        const file = filePath.reverse().join(':').replace(mainDirectory, '.').substring(1);
-        if(file.startsWith('internal/modules/')) break;
-        out.push({file, func: func.startsWith('Object.') ? func.replace('Object.', '') : func, char, line});
-    }
-    return out;    
-}
-
-
 const defaultFormat: NiceLoggerFormatter = ({args, stackInfo}) => {
-    const [{line = '<?>', func = '<?>', file = '<?>'} = {}] = stackInfo;
-    const filePlusLine = `${file}:${line}`
-    return ["\x1b[0m", args.join(' '), '    ',"\x1b[4m", `${filePlusLine}`,"\x1b[0m", ,"\x1b[2m", '\t\t', `ƒ:${func}`, "\x1b[0m"].join('')
+    const [{line = '<?>', file = '<?>'} = {}] = stackInfo;
+    const filePlusLine = `${file}:${line}`    
+    const func = stackInfo.map(({func}) => {
+        return func.startsWith('Object.') 
+            ? func.replace('Object.', '') 
+            : func
+    }).join(' <| ');
+    
+    return ["\x1b[0m", args.join(' '), '\n',"\x1b[4m", `${filePlusLine}`,"\x1b[0m", ,"\x1b[2m", '  \t', `ƒ ${func}`, "\x1b[0m"].join('')
 }
 
 type NiceLogger = NiceLoggerConsole & {
@@ -36,10 +21,38 @@ type NiceLogger = NiceLoggerConsole & {
     filter: NiceLoggerFilter;
     format: NiceLoggerFormatter;
     silent: boolean;
+    pathPrefix: string;
 }
 
-export function createLogger({console: c = console, filter = () => true, format = defaultFormat, silent = false}: {console?: NiceLoggerConsole, filter?: NiceLoggerFilter, format?: NiceLoggerFormatter, silent?: boolean} = {}): NiceLogger {
-    const ctx = {filter, format, silent, console: c};
+export function createStackInfo(pathPrefix: string): NiceStack {
+    const ctx: {stack?: string} = {}
+    const filePath = require.main!.filename.split('/')
+    filePath.pop();
+    const mainDirectory = filePath.join('/');
+    Error.captureStackTrace(ctx);
+    const rows = ctx.stack!.split('\n').splice(3).map(row => row.trim())
+    
+    const out = [];
+    for(const row of rows) {
+        let [position, ...rest] = row.split(' ').reverse().filter(it => !/^\[|at|\]$/.test(it));
+        position = position.substring(1, position.length-1);
+        const func = rest.join(' ')
+        const outerOrInternalModule = !position.startsWith('/')
+        const [char, line, ...filePath] = position.split(':').reverse();        
+        const file = filePath.reverse().join(':').replace(mainDirectory, '');
+        out.push({
+            file: `${pathPrefix}${file}`, 
+            func, 
+            char, 
+            line
+        });
+        if(outerOrInternalModule) break;
+    }
+    return out;
+}
+export function createLogger({console: c = console, filter = () => true, format = defaultFormat, pathPrefix = '.', silent = false}: {console?: NiceLoggerConsole, filter?: NiceLoggerFilter, format?: NiceLoggerFormatter, silent?: boolean, pathPrefix?: string} = {}): NiceLogger {
+    const ctx = {filter, format, silent, pathPrefix, console: c};
+    
     const shouldWriteToConsole: NiceLoggerFilter = (obj) => {
         try {
             return ctx.filter(obj)
@@ -61,21 +74,21 @@ export function createLogger({console: c = console, filter = () => true, format 
     return {
         log(...args: any[]) {
             if(ctx.silent) return;
-            const stackInfo = createStackInfo();
+            const stackInfo = createStackInfo(ctx.pathPrefix);
             if(shouldWriteToConsole({args, stackInfo, func: 'log'})) {
                 ctx.console.log(formatConsoleData({stackInfo, args, func: 'log'}))
             }
         },
         warn(...args: any[]) {
             if(ctx.silent) return;
-            const stackInfo = createStackInfo();
+            const stackInfo = createStackInfo(ctx.pathPrefix);
             if(shouldWriteToConsole({args, stackInfo, func: 'warn'})) {
                 ctx.console.warn(formatConsoleData({stackInfo, args, func: 'warn'}))
             }
         },
         error(...args: any[]) {
             if(ctx.silent) return;
-            const stackInfo = createStackInfo();
+            const stackInfo = createStackInfo(ctx.pathPrefix);
             if(shouldWriteToConsole({args, stackInfo, func: 'error'})) {
                 ctx.console.error(formatConsoleData({stackInfo, args, func: 'error'}))
             }
@@ -103,6 +116,12 @@ export function createLogger({console: c = console, filter = () => true, format 
         },
         set silent(value: boolean) {
             ctx.silent = value;
+        },
+        get pathPrefix() {
+            return ctx.pathPrefix;
+        },
+        set pathPrefix(value: string) {
+            ctx.pathPrefix = value;
         }
     }
 }
---
+++ playground/index.ts
@@ -0,0 +1,3 @@
+import {createLogger} from '../index'
+
+const logger = createLogger({});
